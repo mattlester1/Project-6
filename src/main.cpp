6928d8e22d00adefa2bf73bb133630305ed0c337
Matt Lester <mattlester1@u.boisestate.edu>
2022-11-06 07:42:23 -0700

    update

--- src/main.cpp
+++ src/main.cpp
@@ -5,48 +5,36 @@
 #include <Wire.h>
 #include <math.h>
 
-
-// defining motor and encoder pins
 #define Motor_Pin1 2
 #define Motor_Pin2 3
 Encoder encode(30, 31);
 
-
-float PPR = 405;
-
-
 Adafruit_MPU6050 mpu;
 sensors_event_t a, g, temp;
-float pitch, roll;
-volatile float pitchOffset = 0.0f;
-volatile float rollOffset = 0.0f;
-float prScalar = 7.8;
 
-int incomingByte = 0;
 
-float eror_integ = 0.0;
-
+const float PPR = 405;
+const int zero_speed = 0;
+const float k_p = 1;
+const float k_i = 1.35;
 volatile float ref_theta;
 volatile float motor_theta;
 volatile float thetaError = 0;
 volatile float oldError = 0;
-
-float k_p = 5;
 volatile float p_control = 0;
-float k_i = .01;
 volatile float i_control = 0;
-int oldTime;
-int newTime;
-int deltaT;
-int power;
+volatile int oldTime;
+volatile int newTime;
+int deltaT = 0;
+int power = 0;
 
 void processIMU();
 float processIMUAngle();
 float readMotorAngle();
-float PI_Control();
+float PI_Control(float IMUAngle, float MotorAngle);
 void sendPWM();
 void processIMU();
-void Compensate();
+void Compensate(float AngleDifference, float Speed);
 int PowerControl();
 
 void setup()
@@ -74,18 +62,16 @@ void setup()
   mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
 
   delay(100);
-  // ref_theta = processIMUAngle(); 
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
-  // Serial.println(processIMUAngle());
-  // Serial.println(readMotorAngle());
-  // Serial.print("\t");
-  // Serial.println(readAngle());
-  Compensate();
+  Serial.println(processIMUAngle());
+  Serial.println(readMotorAngle());
+
+  Compensate(PI_Control(processIMUAngle(), readMotorAngle()), PowerControl());
   // processIMU();
+
 }
 
 void processIMU()
@@ -126,35 +112,50 @@ float processIMUAngle()
 
   sensors_event_t a, g, temp;
   mpu.getEvent(&a, &g, &temp);
-  float angleOffSet = -2.0;
+  float angleOffSet = 0;
 
   return ((180 / M_PI) * atan2(a.acceleration.x, a.acceleration.z)) + angleOffSet;
   
 }
+
+/**
+ * @brief reads motor encoder to determine the angle of motor shaft
+ * 
+ * @returns float 
+ */
 
 float readMotorAngle()
 {
   return (encode.read() / PPR) * 360;
 }
 
+/**
+// @brief Includes proportional and Integral control
+// Proportional control simply takes the difference between the IMU angle and motor angle and 
+// multiplies by Proportional constant k_p
+// Integral control finds the difference between the old registerd angled difference (oldError)  
+// and the new angle difference (thetaError) and multiplies that difference by the change in time
+// this is then summed with the previous i_control value and mulitplied by the intergral constant k_i
+// @returns p_control + i_control
+*/
 
-float PI_Control(){
+float PI_Control(float IMUAngle, float MotorAngle){
   newTime = millis();
-  // ref_theta = processIMUAngle();
-  motor_theta = readMotorAngle();
+  ref_theta = IMUAngle;
+  motor_theta = MotorAngle;
 
-  thetaError = ref_theta - motor_theta;
+  thetaError = ref_theta + motor_theta;
   p_control = k_p * thetaError; // Seems to be working, creates occelations, may need refinment
 
   oldTime = newTime;
   oldError = thetaError;
 
   newTime = millis();
-  // ref_theta = processIMUAngle();
-  motor_theta = readMotorAngle();
+  ref_theta = IMUAngle;
+  motor_theta = MotorAngle;
 
   deltaT = (newTime - oldTime) * 1000;
-  thetaError = ref_theta - motor_theta;
+  thetaError = ref_theta + motor_theta;
   i_control = k_i * (i_control + ((thetaError - oldError) * deltaT));
   return p_control + i_control;
 
@@ -162,38 +163,54 @@ float PI_Control(){
 }
 
 
-void Compensate()
+
+/**
+// Takes results from PI_Control() and PowerControl() and applies them to motor
+*/
+
+void Compensate(float AngleDifference, float Speed)
 {
 
-  float comp = -1 * PI_Control();
+  float comp = AngleDifference;
 
+    if (comp == 0){
+      analogWrite(Motor_Pin1, zero_speed);
+      analogWrite(Motor_Pin2, zero_speed);
+    }
+    else if (comp > 0){
 
-
-    if (comp > 0){
-
-      analogWrite(Motor_Pin1, 0);
-      analogWrite(Motor_Pin2, PowerControl());
+      analogWrite(Motor_Pin1, Speed);
+      analogWrite(Motor_Pin2, zero_speed);
      }
 
     else{
-      analogWrite(Motor_Pin1, PowerControl());
-      analogWrite(Motor_Pin2, 0);
+      analogWrite(Motor_Pin1, zero_speed);
+      analogWrite(Motor_Pin2, Speed);
 
     }
   }
 
-  int PowerControl(){
 
-    if (abs(PI_Control()) < 10){
-      power = 100;
-    }
-    else if (abs(PI_Control()) < 20){
+
+/**
+  // controls power output depending on angle difference. 
+  //The greater the difference the the greater the power
+  // @returns power
+*/  
+  
+  int PowerControl(){
+    float angleError = PI_Control(processIMUAngle(), readMotorAngle());
+
+    if (abs(angleError) < 5){
       power = 150;
     }
-    else if (abs(PI_Control()) < 30){
+    else if (abs(angleError) < 10){
+      power = 175;
+    }
+    else if (abs(angleError) < 25){
       power = 200;
     }
-    else if (abs(PI_Control()) < 40){
+    else if (abs(angleError) < 40){
       power = 225;
     }
     else{
@@ -201,6 +218,8 @@ void Compensate()
     }
     return power;
   }
+
+
 
   // while ((abs(IMUAngle - motorAngle)) > 0.5)
   // {
